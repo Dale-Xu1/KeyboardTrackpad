b6c10692aac062e06cb0d36d5a3d0bc80c05e36b
Dale Xu <69087617+Dale-Xu1@users.noreply.github.com>
2020-09-27 10:53:04 -0400

    Keyboard events

--- trackpad/Keyboard.py
+++ trackpad/Keyboard.py
@@ -22,14 +22,12 @@ class Keyboard(tk.Canvas):
         self.create_image(width / 2, height / 2, image=self.image)
 
         # Create keys
-        self.keys = keys
         self.rectangles = {}
+        self.create_keys(keys)
 
-        self.create_keys()
+    def create_keys(self, keys):
 
-    def create_keys(self):
-
-        for key in self.keys:
+        for key in keys:
 
             a = key.a
             b = key.b
@@ -37,3 +35,17 @@ class Keyboard(tk.Canvas):
             # Create rectangle
             rectangle = self.create_rectangle(a.x, a.y, b.x, b.y, outline='green')
             self.rectangles[key.name] = rectangle
+
+    def on_key_down(self, name):
+        self.set_outline(name, 'red')
+
+    def on_key_up(self, name):
+        self.set_outline(name, 'green')
+
+    def set_outline(self, name, color):
+
+        # Set outline
+        if name in self.rectangles:
+
+            rectangle = self.rectangles[name]
+            self.itemconfig(rectangle, outline=color)
--- trackpad/__init__.py
+++ trackpad/__init__.py
@@ -1,4 +1,6 @@
 import csv
+import keyboard
+
 from trackpad.Keyboard import Keyboard
 from trackpad.Key import Key
 from trackpad.Vector import Vector
@@ -15,7 +17,8 @@ class Trackpad:
             reader = csv.reader(csv_file)
 
             for row in reader:
-                # Convert data to object
+
+                # Convert data to an object
                 name = row[0]
                 pos = Vector(float(row[1]), float(row[2]))
 
@@ -29,9 +32,21 @@ class Trackpad:
         self.window = window
         self.keyboard = Keyboard(self.keys)
 
+        # Bind events
+        keyboard.on_press(self.on_key_down, True)
+        keyboard.on_release(self.on_key_up, True)
+
         # Start loop
         window.after(0, self.update)
 
     def update(self):
 
         self.window.after(20, self.update)
+
+    def on_key_down(self, e):
+
+        self.keyboard.on_key_down(e.name)
+
+    def on_key_up(self, e):
+
+        self.keyboard.on_key_up(e.name)
